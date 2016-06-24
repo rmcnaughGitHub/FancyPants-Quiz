c12291640ee7941b256e6520d81dcfab75ba916e
Raymond R-Mcnaught <raymond.richards-mcnaught@grey.com>
2016-06-24 19:17:02 -0400

    fixed fade-in

--- js/QuizWhiz.js
+++ js/QuizWhiz.js
@@ -8,7 +8,8 @@ $(function(){
 	$snoopy = $('.snoopy');
 	$yatch = $('.yatch');
 
-	var current;
+	var current,
+	rightAnswer = false;
 
 	var quizArr = [
 		$one,
@@ -16,22 +17,19 @@ $(function(){
 		$three
 	];
 
-	console.log('Working ',quizArr);
+	//console.log('Working ',quizArr);
 
 	function opacitySwitch(object, percentage) {
 		if( object.hasClass('display-none') ){
 			object.removeClass('display-none').addClass('display-block');
 		}
-		object.animate(function(){
-			opacity: percentage,
-			'swing'
-		}, 300);
-		//console.log('opacity ', object);
+		object.animate({opacity: percentage}, 500);
+		console.log( object, ' ', 'opacity ', object.css('opacity'));
 	}
 
-	function fadeinTime(){
+	function fadeinTime(object){
 		setInterval(function(){
-			opacitySwitch(current, 1);
+			opacitySwitch(object, 1);
 		},100);
 	}
 
@@ -39,23 +37,32 @@ $(function(){
 		var current = $one;
 		//for (var i = 0; i < quizArr.length; i++){
 			
-			if( current == $one){
+			if( current == $one ){
 				opacitySwitch(current, 0);
 				opacitySwitch($marvin, 0);
-				//current = $two;
-				//fadeinTime();
+				current = $two;
+				fadeinTime(current);	
+				console.log('current ',current,' Opacity ', current.css('opacity'));
+			}
+			if ( rightAnswer == true ){
 				
 			}
-			console.log('current ',current,' Opacity ', current.css('opacity'));
+			//console.log('current ',current,' Opacity ', current.css('opacity'));
 		//}
 	}
 
-	opacitySwitch($('.marvin'), 0);
+	//opacitySwitch($('.one'), 0);
+	//opacitySwitch($('.marvin'), 0);
+
+
+
+	//$('.marvin').animate({opacity: 0}, 1000);
 
 	$('.correct').click(function(e){
 		e.preventDefault();
-		//opacitySwitch($next, 1);
+		opacitySwitch($next, 1);
 		switchQuiz();
+		rightAnswer = true;
 		console.log('Correct');
 	});
 
