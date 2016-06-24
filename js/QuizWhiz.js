f50e7df4e2b6aa5d837a46f176bd8d5e0b411f3c
Raymond R-Mcnaught <raymond.richards-mcnaught@grey.com>
2016-06-24 18:12:06 -0400

    -adding functions to activate quiz

--- js/QuizWhiz.js
+++ js/QuizWhiz.js
@@ -1,7 +1,37 @@
 $(function(){
 
-	console.log('Working');
+	$one = $('.one');
+	$two = $('.two');
+	$three = $('.three');
 
+	var quizArr = [
+		$one,
+		$two,
+		$three
+	];
+
+	console.log('Working ',quizArr);
+
+
+	$('.correct').click(function(e){
+		e.preventDefault();
+		console.log('Correct');
+	});
+
+
+	$('.next').click(function(e){
+		e.preventDefault();
+		for (var i = 0; i < quizArr.length; i++){
+			
+			if( i == 0){
+				quizArr[].animate(function(){
+
+				});
+			}
+
+		}
+		console.log('Next');
+	});
 
 
 
