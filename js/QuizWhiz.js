6d5c9c53e0cf092a2fe05a0d940bda7a1568f6a6
Raymond R-Mcnaught <raymond.richards-mcnaught@grey.com>
2016-06-24 20:03:21 -0400

    fixing quiz function

--- js/QuizWhiz.js
+++ js/QuizWhiz.js
@@ -3,12 +3,17 @@ $(function(){
 	$one = $('.one');
 	$two = $('.two');
 	$three = $('.three');
+
 	$next = $('.next');
+
 	$marvin = $('.marvin');
 	$snoopy = $('.snoopy');
 	$yatch = $('.yatch');
 
-	var current,
+	$correctImage = $('.correct-image');
+	$inCorrectImage = $('.incorrect-image');
+
+	var current = $one,
 	rightAnswer = false;
 
 	var quizArr = [
@@ -23,8 +28,8 @@ $(function(){
 		if( object.hasClass('display-none') ){
 			object.removeClass('display-none').addClass('display-block');
 		}
-		object.animate({opacity: percentage}, 500);
-		console.log( object, ' ', 'opacity ', object.css('opacity'));
+		object.animate({opacity: percentage}, 300);
+		//console.log( object, ' ', 'opacity ', object.css('opacity'));
 	}
 
 	function fadeinTime(object){
@@ -34,49 +39,89 @@ $(function(){
 	}
 
 	function switchQuiz(){
-		var current = $one;
 		//for (var i = 0; i < quizArr.length; i++){
-			
+			opacitySwitch($one, 0);
+			opacitySwitch($two, 0);
+			opacitySwitch($three, 0);
 			if( current == $one ){
-				opacitySwitch(current, 0);
+				opacitySwitch($one, 0);
 				opacitySwitch($marvin, 0);
-				current = $two;
-				fadeinTime(current);	
-				console.log('current ',current,' Opacity ', current.css('opacity'));
+				current = $two;	
 			}
+			if( current == $two ){
+				opacitySwitch($two, 0);
+				opacitySwitch($snoopy, 0);
+				current = $three;	
+				///console.log('current ',current,' Opacity ', current.css('opacity'));
+			}
+			if( current == $three ){
+				opacitySwitch($three, 0);
+				opacitySwitch($yatch, 0);
+				current = $one;	
+				//console.log('current ',current,' Opacity ', current.css('opacity'));
+			}
+			console.log('current = ',current,' Opacity ', current.css('opacity'));
+			//
 			if ( rightAnswer == true ){
-				
+				opacitySwitch($correctImage, 1);
+			}else{
+				opacitySwitch($inCorrectImage, 1);
 			}
 			//console.log('current ',current,' Opacity ', current.css('opacity'));
 		//}
 	}
 
-	//opacitySwitch($('.one'), 0);
-	//opacitySwitch($('.marvin'), 0);
-
-
-
-	//$('.marvin').animate({opacity: 0}, 1000);
 
 	$('.correct').click(function(e){
 		e.preventDefault();
-		opacitySwitch($next, 1);
-		switchQuiz();
 		rightAnswer = true;
+		if( current == $three ){
+			opacitySwitch($next, 0);
+		}else{
+			opacitySwitch($next, 1);
+		}	
+		switchQuiz();
 		console.log('Correct');
 	});
+
+	$('.in-correct').click(function(e){
+		e.preventDefault();
+		rightAnswer = false;
+		if( current == $three ){
+			opacitySwitch($next, 0);
+		}else{
+			opacitySwitch($next, 1);
+		}	
+		switchQuiz();
+		console.log('In-Correct');
+	});
+
 
 
 	$('.next').click(function(e){
 		e.preventDefault();
-		for (var i = 0; i < quizArr.length; i++){
+		opacitySwitch($correctImage,0);
+		opacitySwitch($inCorrectImage,0);
+		opacitySwitch($next,0);
+		/*for (var i = 0; i < quizArr.length; i++){
 			
 			if( i == 0){
 				
 			}
 
+		}*/
+		if( current == $one ){
+			opacitySwitch($two, 1);
+			opacitySwitch($snoopy, 1);
+			//$one.removeClass('display-block').addClass('display-none');
+			//$one.removeClass('display-block').addClass('display-none');
+			current = $two;	
 		}
-		switchQuiz();
+		if( current == $two ){
+			opacitySwitch($three, 1);
+			opacitySwitch($yatch, 1);
+			current = $three;	
+		}
 		console.log('Next');
 	});
 
